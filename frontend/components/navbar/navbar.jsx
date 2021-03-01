e71b59a19e30ae1af1df8b2a2e939ea2508d3085
Nikolay <nikoobox@gmail.com>
2021-02-28 22:57:58 -0500

    responsive nav

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
+import { Dropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { HiMenu } from 'react-icons/hi';
+import { CgMenu } from 'react-icons/cg';
 
 class Navbar extends React.Component{
     constructor(props){
@@ -17,13 +21,49 @@ class Navbar extends React.Component{
 
         const whenCurrentUser = !currentUser ? 
             <>
-                <li> <Link to="/" onClick={() => this.props.openModal('signup')} className='navbar-link'>Sign Up</Link>  </li>    
-                <li><Link to="/" onClick={() => this.props.openModal('login')} className='navbar-link'>Log In</Link> </li>
+                <li> <Link to="/" onClick={() => this.props.openModal('signup')} className='navbar-link hide-link'>Sign Up</Link>  </li>    
+                <li><Link to="/" onClick={() => this.props.openModal('login')} className='navbar-link hide-link'>Log In</Link> </li>
+                <Dropdown className='dropdown'>
+                    <Dropdown.Toggle variant="success" id="">
+                        <div className='burger'>
+                            <CgMenu />
+
+                        </div>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <LinkContainer to='/'>
+                            <Dropdown.Item id='' onClick={() => this.props.openModal('signup')}>
+                                Sign Up
+                            </Dropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to='/'>
+                            <Dropdown.Item onClick={() => this.props.openModal('login')}>Log In</Dropdown.Item>
+                        </LinkContainer>
+                    </Dropdown.Menu>
+                </Dropdown>
             </> : 
             <>
-                <li className='navbar-welcome-user' onClick={this.showUser}>Welcome,<span> {currentUser.first_name}</span>!</li>
+                <li className='navbar-welcome-user hide-link' onClick={this.showUser}>Welcome,<span> {currentUser.first_name}</span>!</li>
 
-                <li ><Link to="/"><button onClick={() => logout()} className='navbar-logout-btn'>Log Out</button></Link></li>
+                <li ><Link to="/"><button onClick={() => logout()} className='navbar-logout-btn hide-link'>Log Out</button></Link></li>
+
+                <Dropdown className='dropdown'>
+                    <Dropdown.Toggle variant="success" id="">
+                        <div className='burger'>
+                            <CgMenu />
+                        </div>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        {/* <LinkContainer to='/'> */}
+                            <Dropdown.Item onClick={this.showUser}>{currentUser.first_name}</Dropdown.Item>
+                        {/* </LinkContainer> */}
+                        <LinkContainer to='/'>
+                            <Dropdown.Item id='' onClick={() => logout()}>
+                                Log Out
+                            </Dropdown.Item>
+                        </LinkContainer>
+                    </Dropdown.Menu>
+                </Dropdown>
             </>
 
         return(
